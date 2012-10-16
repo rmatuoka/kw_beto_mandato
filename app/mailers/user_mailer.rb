a92e7a2edd3c437e9a7ac1fae863ca0de0c8b28d
Rafael Matuoka Chiocchetti <rmatuoka@gmail.com>
2012-10-15 22:13:37 -0300

    ajuste de texto

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ActionMailer::Base
   def resposta(survey)
     @survey = survey
     #mail(:to => "rmatuoka@korewa.com.br", :bcc => "bsilva@korewa.com.br, vendas@farmaciaecovita.com.br", :subject => "Mandato Participativo!")
-    mail(:to => "betotricoli@al.sp.gov.br", :bbc => "rmatuoka@korewa.com.br, betrix43@hotmail.com", :subject => "Mandato Participativo!")
+    mail(:to => "deputadobetotricoli@gmail.com", :bbc => "rmatuoka@korewa.com.br, betrix43@hotmail.com", :subject => "Mandato Participativo!")
   end
 end
